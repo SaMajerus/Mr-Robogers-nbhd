dccf705f3d8584f5d44dec529221ce90d00fea4f
Samuel Majerus <ladolego@gmail.com>
2022-08-19 13:02:28 -0700

    'substituteNums' -- Test 3 passed.  (Turns out I had to do a normal 'for' loop this time instead of for-each)

--- js/scripts.js
+++ js/scripts.js
@@ -28,7 +28,7 @@ function substituteNums(currArr) {   //Substitutes numbers containing digits '1'
   let eNum = []; //Empty array variable that will store the 1st, 2nd, 3rd, and 4th digits, respectively, in separate indices. This will allow the logic to evaluate which String-sub to apply (if '1', '2', and/or '3' -- or a combination -- are present in the element) 
   let eCopy;  //'element' will get copied to this variable, so that the original 'element' var can be pushed -- or have something else pushed -- to 'newArr' once the logic does its String-sub Hierarchy evaluation. 
   let eTemp = []; //Initializes temporary array. Saves the results of 'eCopy.split(" ")'. 
-  let inNum = 0; 
+
   //Pre-defining -- and initializing -- constant vars for the 1st, 2nd, 3rd, and 4th digits, respectively, of the given element. 
   const d1=0; 
   const d2=0; 
@@ -36,29 +36,34 @@ function substituteNums(currArr) {   //Substitutes numbers containing digits '1'
   const d4=0; 
 
   //console.log(currArr); 
-  currArr.forEach(function(element) { 
-    inNum = parseInt(element); 
-    console.log("Length of '" + inNum + "' is:  " + inNum.length);
-    if(inNum.length === 1) { 
-      if(inNum === 1) { 
+  
+  for (let index = 0; index < currArr.length; index += 1) {
+    let curr = String(currArr[index]);  //Casts current element as a String, saves to the variable 'curr'. 
+
+    console.log("Length of '" + curr + "' is:  " + curr.length);
+
+    if(curr.length === 1) { 
+      if(curr === 1) { 
         newArr.push(sub1); 
-      } else if (inNum === 2) { 
+      } else if (curr === 2) { 
         newArr.push(sub2); 
-      } else if (inNum === 3) { 
+      } else if (curr === 3) { 
         newArr.push(sub3); 
       } else { 
-        newArr.push(inNum); 
+        newArr.push(curr); 
       } 
-    } else if(inNum.length === 2) { 
-      eCopy = inNum; 
-      eTemp = eCopy.split(""); 
-      for(let i=0; i < eTemp.length; i+=1){ 
+    } else if(curr.length === 2) {  //If number has 2 digits 
+      eCopy = curr; 
+      eTemp = (eCopy.trim("")).split(""); 
+      console.log("Current contents of array 'eTemp' (for currArr element  '" + curr + "'):  " + eTemp); 
+      for (let i=0; i < eTemp.length; i+=1) {  //Copying contents from 'eTemp' to 'eNum' arrays. 
         eNum.push(eTemp[i]); 
       } 
-      d1 = eNum[0]; 
-      console.log("d1 of '" + inNum + "' is:  " + d1); 
-      d2 = eNum[1]; 
-      console.log("d2 of '" + inNum + "' is:  " + d2); 
+      console.log("Current contents of array 'eNum' (for currArr element  '" + curr + "'):  " + eNum);
+      //d1 = eNum[0]; 
+      console.log("d1 of '" + curr + "' is:  " + eNum[0]); 
+      //d2 = eNum[1]; 
+      console.log("d2 of '" + curr + "' is:  " + eNum[1]); 
       /*
       if(d1 === 1){
         if(d2 === 2){
@@ -66,11 +71,10 @@ function substituteNums(currArr) {   //Substitutes numbers containing digits '1'
         }
       }
       */ 
-      newArr.push(inNum);  //This is temporary; it'll hopefully ensure that no errors get thrown when testing if code on Lines 49-53 [chiefly 49] work. 
+      newArr.push(curr);  //This is temporary; it'll hopefully ensure that no errors get thrown when testing if code on Lines 56-58 [chiefly 57] work. 
     }
 
-  }); 
-
+  }
   //console.log("Final form of 'newArr': " + newArr); 
   return newArr; 
 } 
